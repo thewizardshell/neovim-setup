be1544ede3aecdc12d0f8705a9ddd3b9fffb3220
Vicente Roa <vicenteroa03@gmail.com>
2024-06-09 19:38:25 -0400

    Update Config : Prettier Astro
    Configuracion de prettier para soportar el lenguaje astro

--- lua/plugins/astro.lua
+++ lua/plugins/astro.lua
@@ -10,4 +10,33 @@ return {
       },
     },
   },
+  {
+    "conform.nvim",
+    opts = function(_, opts)
+      if LazyVim.has_extra("formatting.prettier") then
+        opts.formatters_by_ft = opts.formatters_by_ft or {}
+        opts.formatters_by_ft.astro = { "prettier" }
+      end
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        astro = {},
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@astrojs/ts-plugin",
+          location = LazyVim.get_pkg_path("astro-language-server", "/node_modules/@astrojs/ts-plugin"),
+          enableForWorkspaceTypeScriptVersions = true,
+        },
+      })
+    end,
+  }
 }
