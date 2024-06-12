f0a3e708ad4a08dbd6b5cd2a3d836791164c0a1a
Vicente Roa <vicenteroa03@gmail.com>
2024-06-12 00:02:26 -0400

    Keymaps: Added Telescope Live Grep
    He agregado un comando que facilita un poco mas el uso de grep al
    momento de trabajar en proyectos

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- keymaps.lua
+local map = vim.api.nvim_set_keymap
+local options = { noremap = true, silent = true }
+
+map('n', '<leader>G', '<cmd>Telescope live_grep<CR>', options)
