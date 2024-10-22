212ee2c8ca25a7023a2ac4bd6cf4673e2a636197
vicenteroa <vicenteroa03@gmail.com>
2024-10-22 12:58:34 -0300

    Updgrade: new ui + plugins
    - Added plugins rose-pine (actual theme)
    - modify telescope
    - modify ui and added icon harmony in japan

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,68 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  requires = { { "nvim-lua/plenary.nvim" } }, -- Dependencia requerida por Telescope
+
+  config = function()
+    local telescope = require("telescope")
+
+    -- Configuración básica de Telescope
+    telescope.setup({
+      defaults = {
+        -- Configuraciones generales
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+        },
+        prompt_prefix = "🔍 ",
+        selection_caret = " ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "descending",
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            mirror = false,
+            preview_width = 0.5,
+            prompt_position = "top",
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.75,
+          height = 0.85,
+          preview_cutoff = 120,
+        },
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
+        file_ignore_patterns = {},
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        path_display = { "truncate" },
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        color_devicons = true,
+        use_less = true,
+        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+      },
+
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = false, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        },
+      },
+    })
+
+    telescope.load_extension("fzf")
+  end,
+}
