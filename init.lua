434d2db32273d94b20c0bb33dc303ecc06c5dfd1
vicenteroa <vicenteroa03@gmail.com>
2024-11-05 15:14:18 -0300

    Update(init,lazy): Updates last weeks and delete comments

--- init.lua
+++ init.lua
@@ -2,6 +2,3 @@
 require("config.lazy")
 vim.opt.spell = true
 vim.opt.spelllang = "es"
--- vim.cmd([[highlight Normal guibg=NONE]])
--- vim.cmd([[highlight NonText guibg=NONE]])
--- vim.cmd([[highlight LineNr guibg=NONE]])
